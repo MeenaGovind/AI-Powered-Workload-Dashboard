0f83db11ee335a58f2aafaeb9eb502796e37b7c9
Govind Meena <raghavigovind2216@gmail..com>
2025-12-23 15:25:15 +0530

    Firebase.js updated

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,9 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccount.json" assert { type: "json" };
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "serviceAccount.json"), "utf8")
-);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-export default serviceAccount;
+export const db = admin.firestore();
